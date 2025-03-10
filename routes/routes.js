a25eb14da7a5ce2a1379cb6d41b30c0213624444
Naifozz <sdebraeckelaer@gmail.com>
2025-03-10 14:31:38 +0100

    ROUTE AUTEUR & EMPRUNT

--- routes/routes.js
+++ routes/routes.js
@@ -1,32 +1,77 @@
 // routes/routes.js
-import { livreController } from "../controllers/livreController.js";
-import { auteurController } from "../controllers/auteurController.js";
-import { empruntController } from "../controllers/empruntController.js";
+import {
+    livreGetAll,
+    getLivreById,
+    controllersCreateLivre,
+    controllersUpdateLivre,
+    controllersDeleteLivre,
+} from "../controllers/livreController.js";
+import * as auteurController from "../controllers/auteurController.js";
+import {
+    getEmpruntById,
+    empruntGetAll,
+    controllersCreateEmprunt,
+    controllersUpdateEmprunt,
+    controllersDeleteEmprunt,
+} from "../controllers/empruntController.js";
 import { logger } from "../utils/logger.js";
+import { parseRequestBody } from "../utils/httpHelper.js";
 
-export const routes = (req, res) => {
+export const routes = async (req, res) => {
     const url = req.url;
     const method = req.method;
 
     // Routes pour les livres
     if (url === "/api/livres" && method === "GET") {
-        livreController.getAllLivres(req, res);
+        const livres = await livreGetAll();
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify(livres.data));
     } else if (url === "/api/livres" && method === "POST") {
-        livreController.createLivre(req, res);
+        const livreData = await parseRequestBody(req);
+
+        const result = await controllersCreateLivre(livreData);
+
+        if (!result.success) {
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(result.error));
+        }
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify("Données envoyées", result.data));
     } else if (url.match(/^\/api\/livres\/([0-9]+)$/) && method === "GET") {
         const id = url.split("/")[3];
-        livreController.getLivreById(req, res, parseInt(id));
+        const livre = await getLivreById(id);
+
+        if (!livre.success) {
+            res.writeHead(400, { "Content-type": "application/json" });
+            res.end(JSON.stringify(livre.error));
+        }
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify(livre.data));
     } else if (url.match(/^\/api\/livres\/([0-9]+)$/) && method === "PUT") {
         const id = url.split("/")[3];
-        livreController.updateLivre(req, res, parseInt(id));
+        const livreData = await parseRequestBody(req);
+
+        const result = await controllersUpdateLivre(id, livreData);
+
+        if (!result.success) {
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(result.error));
+        }
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify("Données envoyées", result.data));
     } else if (url.match(/^\/api\/livres\/([0-9]+)$/) && method === "DELETE") {
         const id = url.split("/")[3];
-        livreController.deleteLivre(req, res, parseInt(id));
+        const result = await controllersDeleteLivre(id);
+
+        res.end(JSON.stringify(result.message));
     }
 
     // Routes pour les auteurs
     else if (url === "/api/auteurs" && method === "GET") {
-        auteurController.getAllAuteurs(req, res);
     } else if (url === "/api/auteurs" && method === "POST") {
         auteurController.createAuteur(req, res);
     } else if (url.match(/^\/api\/auteurs\/([0-9]+)$/) && method === "GET") {
@@ -41,18 +86,51 @@ export const routes = (req, res) => {
 
         // Routes pour les emprunts
     } else if (url === "/api/emprunts" && method === "GET") {
-        empruntController.getAllEmprunts(req, res);
+        const emprunts = await empruntGetAll();
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify(emprunts.data));
     } else if (url === "/api/emprunts" && method === "POST") {
-        empruntController.createEmprunt(req, res);
+        const empruntData = await parseRequestBody(req);
+
+        const result = await controllersCreateEmprunt(empruntData);
+
+        if (!result.success) {
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(result.error));
+        }
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify("Données envoyées", result.data));
     } else if (url.match(/^\/api\/emprunts\/([0-9]+)$/) && method === "GET") {
         const id = url.split("/")[3];
-        empruntController.getEmpruntById(req, res, parseInt(id));
+        const emprunt = await getEmpruntById(id);
+
+        if (!emprunt.success) {
+            res.writeHead(400, { "Content-type": "application/json" });
+            res.end(JSON.stringify(emprunt.error));
+        }
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify(emprunt.data));
     } else if (url.match(/^\/api\/emprunts\/([0-9]+)$/) && method === "PUT") {
         const id = url.split("/")[3];
-        empruntController.updateEmprunt(req, res, parseInt(id));
+        const empruntData = await parseRequestBody(req);
+
+        const result = await controllersUpdateEmprunt(id, empruntData);
+
+        if (!result.success) {
+            res.writeHead(400, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(result.error));
+        }
+
+        res.writeHead(201, { "Content-type": "application/json" });
+        res.end(JSON.stringify("Données envoyées", result.data));
     } else if (url.match(/^\/api\/emprunts\/([0-9]+)$/) && method === "DELETE") {
         const id = url.split("/")[3];
-        empruntController.deleteEmprunt(req, res, parseInt(id));
+        const result = await controllersDeleteEmprunt(id);
+
+        res.end(JSON.stringify(result.message));
     }
     // Route non trouvée
     else {
